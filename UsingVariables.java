df7a2f394dd889b6e0330694333e7a39c87224dd
Jonathan Scialpi <jonathanscialpi@gmail.com>
2015-09-09 22:13:48 -0400

    Update UsingVariables.java

--- UsingVariables.java
+++ UsingVariables.java
@@ -6,10 +6,12 @@ public class UsingVariables {
       */
       public static void main(String[] args) {
           
+            // define the three values
     	  	int firstValue = 113;
     	  	double secondValue = 2.71828;
     	  	String thirdValue = "Computer Science";
     	  	
+    	  	//use the concatenation symbol '+' to print them with a String
     	  	System.out.println("This is room #"+firstValue);
     	  	System.out.println("e is close to "+secondValue);
     	  	System.out.println("I am learning a bit about "+thirdValue);
