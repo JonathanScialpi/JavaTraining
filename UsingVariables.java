c3856fd4b9c61afe851c78d7c8608f5da9b8a2bf
Jonathan Scialpi <jonathanscialpi@gmail.com>
2015-09-09 22:12:22 -0400

    Create UsingVariables.java

---
+++ UsingVariables.java
@@ -0,0 +1,18 @@
+
+public class UsingVariables {
+
+      /**
+      * @param args
+      */
+      public static void main(String[] args) {
+          
+    	  	int firstValue = 113;
+    	  	double secondValue = 2.71828;
+    	  	String thirdValue = "Computer Science";
+    	  	
+    	  	System.out.println("This is room #"+firstValue);
+    	  	System.out.println("e is close to "+secondValue);
+    	  	System.out.println("I am learning a bit about "+thirdValue);
+            }
+
+}
