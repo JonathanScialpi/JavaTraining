e820e232248a4579b67547f9945df59ac015865e
Jonathan Scialpi <jonathanscialpi@gmail.com>
2015-09-09 22:21:21 -0400

    Create UsingVariablesAgain.java

---
+++ UsingVariablesAgain.java
@@ -0,0 +1,17 @@
+
+public class UsingVariablesAgain {
+
+      /**
+      * @param args
+      */
+      public static void main(String[] args) {
+          
+    	  	String name = "Jonathan Edward Scialpi";
+    	  	int gradYear = 2012;
+    	  	
+    	  	/*take note of the concatenation symbol '+' between strings and variables as well
+    	  	  as the single quotes around the period since it is simply a character */
+    	  	System.out.println("My name is " + name + "and I'll graduate in " + gradYear+'.');
+            }
+
+}
