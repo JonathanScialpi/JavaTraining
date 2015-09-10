8aefb62fd1fd764fc9d590978b7fb320b953ce2c
Jonathan Scialpi <jonathanscialpi@gmail.com>
2015-09-09 21:23:06 -0400

    Create YourInitials.java

---
+++ YourInitials.java
@@ -0,0 +1,28 @@
+
+public class YourInitials {
+
+      /**
+      * @param args
+      */
+      public static void main(String[] args) {
+          
+        // store each line of output as an element of an array of Strings
+    	  	String[] initialChars = {
+    	  			"For the name Jonathan Edward Scialpi...",
+    	  			"",
+    	  			"JJJJJ EEEEEE  SSS",
+    	  			"  J   E      S   S",
+    	  			"  J   E      S",
+    	  			"  J   EEE     SSS",
+    	  			"J J   E          S",
+    	  			"J J   E      S   S",
+    	  			" JJ   EEEEE   SSS",
+    	  	};
+    	  //Print each element of initialsChars array
+    	  	for (int i = 0; i < initialChars.length; i++){
+    	  		System.out.println(initialChars[i]);
+    	  	}
+            
+            }
+
+}
