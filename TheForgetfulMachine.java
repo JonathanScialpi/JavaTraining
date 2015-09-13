e05e258712b5d9616e5f05440ead530a55b31a75
Jonathan Scialpi <jonathanscialpi@gmail.com>
2015-09-12 22:11:49 -0400

    Create TheForgetfulMachine.java

---
+++ TheForgetfulMachine.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TheForgetfulMachine {
+
+    // Multi-dimensional array to show Subject in one column and Teacher name in the other
+    public static void main(String[] args) {
+    
+    //Scanner variable, but never storing the user input...
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Give me a word!");
+        keyboard.nextLine();
+        System.out.println("Give me another word!");
+        keyboard.nextLine();
+        System.out.println("Great, now your favorite number?");
+        keyboard.nextLine();
+        System.out.println("And your second-favorite number...");
+        keyboard.nextLine();
+        System.out.println("Whew! Wasn't that fun?");
+
+    }
+
+}
