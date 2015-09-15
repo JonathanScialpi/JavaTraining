a5f640965c091b48349a94bc9bf0381e975b994b
Jonathan Scialpi <jonathanscialpi@gmail.com>
2015-09-15 15:14:18 -0400

    Update and rename TwoMoreQuestionsAgain.java to TwoMoreQuestions.java

--- TwoMoreQuestions.java
+++ TwoMoreQuestions.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TwoMoreQuestionsAgain {
+public class TwoMoreQuestions {
 
     public static void main(String[] args) {
      Scanner keyboard = new Scanner(System.in);
