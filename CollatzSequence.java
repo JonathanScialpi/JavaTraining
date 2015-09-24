3033059ba1c1fc9708f99c4fde5b4232f70f01bc
Jonathan Scialpi <jonathanscialpi@gmail.com>
2015-09-23 23:57:51 -0400

    Update CollatzSequence.java

--- CollatzSequence.java
+++ CollatzSequence.java
@@ -5,6 +5,7 @@ public class CollatzSequence {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
         int steps =0;
+        int largest = Integer.MIN_VALUE;
         System.out.print("Starting Number: ");
         int number = keyboard.nextInt();
         System.out.print(number);
@@ -17,7 +18,9 @@ public class CollatzSequence {
            }
            System.out.print("   "+number);
            ++steps;
+           largest = Math.max(number, largest);
        }while(number!=1);
-    System.out.print("\r\nTerminated after "+steps+" steps.");
+    System.out.print("\r\nTerminated after "+steps+" steps." +
+            "\r\n The largest value was: "+largest);
     }
 }
